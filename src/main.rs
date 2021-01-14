c9123437f9300596fc2d73da45c8871dac8685fe
Cole Mickens <cole.mickens@gmail.com>
2021-01-14 13:50:26 -0800

    msal

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,14 @@ arg_enum! {
     }
 }
 
+arg_enum! {
+    #[derive(Debug)]
+    enum AuthProvider {
+        Google,
+        Microsoft,
+    }
+}
+
 #[derive(Debug, structopt::StructOpt)]
 #[structopt(name = "gxt", about = "gxt")]
 struct Args {
@@ -32,22 +40,21 @@ struct Args {
     #[structopt(short = "s", long = "client-secret-path")]
     client_secret_path: String,
 
+    #[structopt(short = "p", long = "provider", default_value = "google", possible_values = &AuthMethod::variants(), case_insensitive = true)]
+    provider: AuthProvider,
+
     #[structopt(long = "login")]
     login_mode: bool,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::from_args();
-    let xdg_dirs = xdg::BaseDirectories::with_prefix("get-xoauth2-token").unwrap();
-
-    let app_secret = yup_oauth2::read_application_secret(&args.client_secret_path)
+async fn get_google_access_token(client_secret_path: &str, username: &str, auth_method: AuthMethod, login_mode: bool, xdg_dirs: xdg::BaseDirectories) -> Result<yup_oauth2::AccessToken, ()> {
+    let app_secret = yup_oauth2::read_application_secret(client_secret_path)
         .await
-        .expect(&format!("failed to read app secret from {}", &args.client_secret_path));
+        .expect(&format!("failed to read app secret from {}", client_secret_path));
 
     let alphabet = base32::Alphabet::Crockford{};
     let safe_client_id = base32::encode(alphabet, app_secret.client_id.clone().as_bytes());
-    let safe_username = base32::encode(alphabet,&args.username.as_bytes());
+    let safe_username = base32::encode(alphabet,username.as_bytes());
 
     let mut cache_path = xdg_dirs.get_cache_home();
     cache_path.push("token-cache");
@@ -56,7 +63,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     cache_path.push(safe_username);
     let token_cache_path = cache_path.as_path();
 
-    if tokio::fs::metadata(token_cache_path).await.is_err() && !args.login_mode {
+    if tokio::fs::metadata(token_cache_path).await.is_err() && !login_mode {
         println!("You have not authenticated. You need to run this:\n");
         let mut cmd: Vec<String> = std::env::args().collect();
         cmd.push("--login".to_string());
@@ -64,7 +71,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::process::exit(-1);
     }
 
-    let method = match args.auth_method {
+    let method = match auth_method {
         AuthMethod::Redirect =>  yup_oauth2::InstalledFlowReturnMethod::HTTPRedirect,
         AuthMethod::Interactive =>  yup_oauth2::InstalledFlowReturnMethod::Interactive,
     };
@@ -79,7 +86,67 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .expect("failed to get access_token from cached refresh_token");
 
-    let xoauth_token = make_xoauth2_token(&args.username, access_token.as_str());
+    return Ok(access_token);
+}
+
+async fn get_microsoft_access_token(client_secret_path: &str, username: &str, auth_method: AuthMethod, login_mode: bool, xdg_dirs: xdg::BaseDirectories) -> Result<yup_oauth2::AccessToken, ()> {
+    let app_secret = yup_oauth2::read_application_secret(client_secret_path)
+        .await
+        .expect(&format!("failed to read app secret from {}", client_secret_path));
+
+    let alphabet = base32::Alphabet::Crockford{};
+    let safe_client_id = base32::encode(alphabet, app_secret.client_id.clone().as_bytes());
+    let safe_username = base32::encode(alphabet,username.as_bytes());
+
+    let mut cache_path = xdg_dirs.get_cache_home();
+    cache_path.push("token-cache");
+    cache_path.push(safe_client_id);
+    tokio::fs::create_dir_all(cache_path.as_path()).await.unwrap();
+    cache_path.push(safe_username);
+    let token_cache_path = cache_path.as_path();
+
+    if tokio::fs::metadata(token_cache_path).await.is_err() && !login_mode {
+        println!("You have not authenticated. You need to run this:\n");
+        let mut cmd: Vec<String> = std::env::args().collect();
+        cmd.push("--login".to_string());
+        println!("   {}", cmd.join(" "));
+        std::process::exit(-1);
+    }
+
+    let method = match auth_method {
+        AuthMethod::Redirect =>  yup_oauth2::InstalledFlowReturnMethod::HTTPRedirect,
+        AuthMethod::Interactive =>  yup_oauth2::InstalledFlowReturnMethod::Interactive,
+    };
+    let authenticator = yup_oauth2::InstalledFlowAuthenticator::builder(app_secret, method)
+        .persist_tokens_to_disk(token_cache_path)
+        .build()
+        .await
+        .expect("failed to create authenticator");
+
+    let access_token = authenticator
+        .token(&["https://mail.google.com"])
+        .await
+        .expect("failed to get access_token from cached refresh_token");
+
+    return Ok(access_token);
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::from_args();
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("get-xoauth2-token").unwrap();
+
+    let access_token = match &args.provider {
+        AuthProvider::Google => { 
+            let token = get_google_access_token(&args.client_secret_path, &args.username, args.auth_method, args.login_mode, xdg_dirs);
+            token.await.unwrap().as_str().to_string()
+        },
+        AuthProvider::Microsoft => { 
+            let token = get_microsoft_access_token(&args.client_secret_path, &args.username, args.auth_method, args.login_mode, xdg_dirs);
+            token.await.unwrap().as_str().to_string()
+        },
+    };
+    let xoauth_token = make_xoauth2_token(&args.username, &access_token);
     print!("{}", xoauth_token);
     Ok(())
 }
